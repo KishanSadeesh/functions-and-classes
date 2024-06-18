d3c6fe185f789ab65b88267c58411514e403835d
KishanSadeesh <131572971+KishanSadeesh@users.noreply.github.com>
2024-06-18 20:29:43 +0530

    fun calling directly(without using objects)

---
+++ app/src/main/java/com/example/threadskotlinapp/Myfunction.kt
@@ -0,0 +1,17 @@
+package com.example.threadskotlinapp
+
+
+class Myfunction {
+
+    fun multiply(a: Int, b:Int)
+    {
+        println(a*b)
+    }
+
+}
+
+fun main()
+{
+    Myfunction().multiply(4,5)
+
+}
